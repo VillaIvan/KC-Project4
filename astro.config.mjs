d9170bf23b51ff2d39ad4da5726d5d8f484f5ee6
Ivan Villa <villaivandev@gmail.com>
2025-05-15 19:15:37 -0300

    astro config cambio en site y base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://VillaIvan.github.io",
+  base: "/KC-Project4",
   vite: {
     plugins: [tailwindcss()],
   },
